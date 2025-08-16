79c0e7a5e5efbf35055b68bf60679006b545e3e3
Ashraf <ashrafhussain2265@gmail.com>
2025-08-16 14:08:22 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [data , setData] = useState([]);
+  const [currentPage , setCurrentPage] = useState(1);
+  const rowsPerPage = 10;
+
+  useEffect(()=>{
+    const pageData = async ()=>{
+        try {     let response = await fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json");
+             if(!response.ok){
+              throw new Error("Error getting the data");
+             }
+             let tData = await response.json();
+             setData(tData);}
+             catch (error){
+              alert("failed to fetch data");
+               console.error(error);
+             }
+    };
+    pageData();
+  },[]);
+
+  //Pagination Calculations ..
+  const totalPages = Math.ceil(data.length/rowsPerPage);
+  const startIndex = (currentPage - 1) * rowsPerPage;
+  const paginatedData = data.slice(startIndex , startIndex + rowsPerPage);
+
+  //handle buttons ..
+   const handlePrevious = ()=>{
+    setCurrentPage((prev)=>Math.max(prev - 1 , 1));
+   }
+  const handleNext = ()=>{
+    setCurrentPage((prev)=>Math.min(prev + 1 , totalPages));
+  };
+  return (
+    <div className="App">
+      Xpagination
+        <table className="page_table">
+  <thead>
+    <tr className='table_head_row'>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Role</th>
+    </tr>
+  </thead>
+  <tbody>
+      {paginatedData.map((ele,idx)=>(
+        <tr key={ele.id}>
+          <td>{ele.id}</td>
+          <td>{ele.name}</td>
+          <td>{ele.email}</td>
+          <td>{ele.role}</td>
+          </tr>
+      ))}
+  </tbody>
+</table>
+<div className="button_group" >
+  <button className='btns' onClick={handlePrevious} disabled={currentPage===1}>previous</button>
+  <span style={{margin : "0 10"}}>
+    {currentPage}/{totalPages} </span>
+  <button className='btns' onClick={handleNext} disabled={currentPage===totalPages}>next</button>
+</div>
+    </div>
+  );
+}
+
+export default App;
